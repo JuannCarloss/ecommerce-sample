f3884e744b58c10d3ead93c270a48b5278b0016e
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2024-06-04 21:27:29 -0300

    feat: repositories classes

---
+++ ecommerce/src/main/java/com/shop/ecommerce/repositories/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.shop.ecommerce.repositories;
+
+import com.shop.ecommerce.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByEmail(String email);
+}
