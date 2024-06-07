686c32464de0cf83f76a1b8e7c5f88de738116c3
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-06 23:01:38 -0300

    feat: ProductController Post method

---
+++ ecommerce/src/main/java/com/shop/ecommerce/resource/ProductController.java
@@ -0,0 +1,25 @@
+package com.shop.ecommerce.resource;
+
+import com.shop.ecommerce.models.Product;
+import com.shop.ecommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping
+    private ResponseEntity save(@RequestBody Product entity){
+        var save = service.post(entity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(save);
+    }
+}
