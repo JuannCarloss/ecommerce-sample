b1a2d2888ee7e5cde985ccc4576b23c2bb4e5153
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-08 00:43:44 -0300

    feat: exception handling for items with quantity less than one

--- ecommerce/src/main/java/com/shop/ecommerce/services/ProductService.java
+++ ecommerce/src/main/java/com/shop/ecommerce/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.shop.ecommerce.services;
 
+import com.shop.ecommerce.enterprise.ValidationException;
 import com.shop.ecommerce.models.Product;
 import com.shop.ecommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ public class ProductService {
     private ProductRepository repository;
 
     public Product post(Product entity){
+        if (entity.getStock() <= 0 ){
+            throw new ValidationException("Products with quantity less than one cannot be registered");
+        }
         return repository.save(entity);
     }
 }
