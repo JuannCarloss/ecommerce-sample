9550edec2a5ac6823c8e3d0ea0e09d22ed10fc03
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-09 19:41:18 -0300

    feat: update product method

--- ecommerce/src/main/java/com/shop/ecommerce/services/ProductService.java
+++ ecommerce/src/main/java/com/shop/ecommerce/services/ProductService.java
@@ -1,10 +1,13 @@
 package com.shop.ecommerce.services;
 
+import com.shop.ecommerce.enterprise.NotFoundException;
 import com.shop.ecommerce.enterprise.ValidationException;
 import com.shop.ecommerce.models.Product;
 import com.shop.ecommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -17,5 +20,23 @@ public class ProductService {
             throw new ValidationException("Products with quantity less than one cannot be registered");
         }
         return repository.save(entity);
+    }
+
+    public Product updateProduct(Long id, Product updatedProduct){
+        Optional<Product> byId = repository.findById(id);
+
+        if (byId.isPresent()){
+
+            var product = Product.builder()
+                    .name(updatedProduct.getName())
+                    .price(updatedProduct.getPrice())
+                    .description(updatedProduct.getDescription())
+                    .stock(updatedProduct.getStock())
+                    .build();
+            product.setId(byId.get().getId());
+            return repository.save(product);
+        }
+
+        throw new NotFoundException("Product not found");
     }
 }
