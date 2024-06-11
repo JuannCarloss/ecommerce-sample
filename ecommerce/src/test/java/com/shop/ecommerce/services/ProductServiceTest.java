7af02d481b60f19373fc2ae00f5aec1dbcd30980
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2024-06-11 12:55:22 -0300

    feat: unit testing ProductService methods

---
+++ ecommerce/src/test/java/com/shop/ecommerce/services/ProductServiceTest.java
@@ -0,0 +1,34 @@
+package com.shop.ecommerce.services;
+
+import com.shop.ecommerce.models.Product;
+import com.shop.ecommerce.repositories.ProductRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Test
+    @DisplayName("Should save a new product in database")
+    public void saveNewProduct(){
+        productService.post(newProduct());
+    }
+
+    public Product newProduct(){
+        return new Product("produto", "descricao", 100.0, 4);
+    }
+
+}
