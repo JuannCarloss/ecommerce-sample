e3e350cca454d50804c1d8be6aa72909b1361b5b
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-15 21:21:30 -0300

    feat: No content exception handler

---
+++ ecommerce/src/main/java/com/shop/ecommerce/enterprise/NoContentException.java
@@ -0,0 +1,7 @@
+package com.shop.ecommerce.enterprise;
+
+public class NoContentException extends RuntimeException{
+    public NoContentException(String message){
+        super(message);
+    }
+}
