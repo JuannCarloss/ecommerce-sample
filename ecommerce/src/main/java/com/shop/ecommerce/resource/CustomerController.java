1e5f87245931cfdf9664f0920ab4451734ad4ef2
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-15 21:18:22 -0300

    feat: implement methods get by id, delete by id and update with documentation

--- ecommerce/src/main/java/com/shop/ecommerce/resource/CustomerController.java
+++ ecommerce/src/main/java/com/shop/ecommerce/resource/CustomerController.java
@@ -25,7 +25,7 @@ public class CustomerController extends AbstractController{
     @Tag(name = "CUSTOMER")
     @Operation(summary = "Post a new customer")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Created a new customer",
+            @ApiResponse(responseCode = "201", description = "Create a new customer",
                     content = { @Content(mediaType = "application/json",
                             schema = @Schema(implementation = Customer.class)) }),
             @ApiResponse(responseCode = "422", description = "the body that you sent is not valid",
@@ -36,11 +36,57 @@ public class CustomerController extends AbstractController{
         return ResponseEntity.status(HttpStatus.CREATED).body(save);
     }
 
+    @Tag(name = "CUSTOMER")
+    @Operation(summary = "Update a customer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Customer updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Customer.class)) }),
+            @ApiResponse(responseCode = "404", description = "Customer not found",
+                    content = @Content) })
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable("id") Long id, Customer entity){
+        var update = service.update(id, entity);
+        return ResponseEntity.ok(update);
+    }
 
     @Tag(name = "CUSTOMER")
+    @Operation(summary = "List all customers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All Customers in database",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Customer.class)) }),
+            @ApiResponse(responseCode = "204", description = "no content in database",
+                    content = @Content) })
     @GetMapping
     public ResponseEntity findAll(){
         List<Customer> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+
+    @Tag(name = "CUSTOMER")
+    @Operation(summary = "Return one customer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "One customer from database",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Customer.class)) })})
+    @GetMapping("{id}")
+    public ResponseEntity byId(@PathVariable("id") Long id){
+        var customer = service.findById(id);
+
+        return ResponseEntity.ok(customer);
+    }
+
+    @Tag(name = "CUSTOMER")
+    @Operation(summary = "Delete one customer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Delete one customer",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Customer.class)) })})
+    @DeleteMapping("{id}")
+    public ResponseEntity deleteById(@PathVariable("id")Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
