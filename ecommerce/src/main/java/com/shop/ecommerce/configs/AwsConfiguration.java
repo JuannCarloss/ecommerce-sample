c806885a22f7748571ec0928381f664c9e73c945
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-16 20:10:49 -0300

    feat: AWS S3 configuration

---
+++ ecommerce/src/main/java/com/shop/ecommerce/configs/AwsConfiguration.java
@@ -0,0 +1,21 @@
+package com.shop.ecommerce.configs;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsConfiguration {
+
+    @Bean
+    public AmazonS3 amazonS3(){
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+}
