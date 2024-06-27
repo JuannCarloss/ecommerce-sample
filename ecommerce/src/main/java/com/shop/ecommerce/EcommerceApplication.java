2dfbb8ae9d2ff3e2079b97cba2c4f8b45a24061d
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-27 00:23:46 -0300

    feat: implementing EnableWebMvc and EnableCaching annotations

--- ecommerce/src/main/java/com/shop/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/shop/ecommerce/EcommerceApplication.java
@@ -2,8 +2,12 @@ package com.shop.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableCaching
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
