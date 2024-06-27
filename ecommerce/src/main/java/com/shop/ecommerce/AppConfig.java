c27166f7fb41820d050f005b47a082be673ecb27
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-27 00:22:33 -0300

    feat: registering RateLimiterInterceptor as a WebMvcInterceptor

---
+++ ecommerce/src/main/java/com/shop/ecommerce/AppConfig.java
@@ -0,0 +1,22 @@
+package com.shop.ecommerce;
+
+import com.shop.ecommerce.configs.RateLimiterInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+
+    private final RateLimiterInterceptor interceptor;
+
+    public AppConfig(RateLimiterInterceptor interceptor) {
+        this.interceptor = interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor);
+    }
+}
