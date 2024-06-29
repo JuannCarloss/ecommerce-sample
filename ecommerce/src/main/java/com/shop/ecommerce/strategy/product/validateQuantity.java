ece28009a2fa6e0dc9317b187e6a46208e670fed
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-28 21:15:44 -0300

    feat: implementing quantity validation

---
+++ ecommerce/src/main/java/com/shop/ecommerce/strategy/product/validateQuantity.java
@@ -0,0 +1,16 @@
+package com.shop.ecommerce.strategy.product;
+
+import com.shop.ecommerce.dtos.ProductRequestDTO;
+import com.shop.ecommerce.enterprise.ValidationException;
+import com.shop.ecommerce.strategy.NewProductValidationStrategy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class validateQuantity implements NewProductValidationStrategy {
+    @Override
+    public void validate(ProductRequestDTO data) {
+        if(data.stock() <= 0){
+            throw new ValidationException("Products quantity must be greater than zero");
+        }
+    }
+}
