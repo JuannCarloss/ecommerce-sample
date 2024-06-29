0fcb41484c11ec2c29df3c39342af3c1b0887cb6
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-28 21:35:44 -0300

    refact: MultipartfileConverter util class

---
+++ ecommerce/src/main/java/com/shop/ecommerce/utils/MultipartfileConverter.java
@@ -0,0 +1,19 @@
+package com.shop.ecommerce.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+public class MultipartfileConverter {
+
+    public static File convertToFile(MultipartFile multipartFile) throws IOException {
+        File convFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        return convFile;
+    }
+}
