2aebb502e7980402a5c339001c0eb1478b5aa840
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-06-28 21:32:37 -0300

    feat: S3Service class

---
+++ ecommerce/src/main/java/com/shop/ecommerce/services/S3Service.java
@@ -0,0 +1,33 @@
+package com.shop.ecommerce.services;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.shop.ecommerce.enterprise.ValidationException;
+import com.shop.ecommerce.utils.MultiparfileConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    @Autowired
+    private AmazonS3Client s3Client;
+    private static final String BUCKET_NAME = "ecommerce-products-imgs";
+
+    public String uploadImg(MultipartFile multipartFile){
+        String filename = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        try{
+            File file = MultiparfileConverter.convertToFile(multipartFile);
+            s3Client.putObject(BUCKET_NAME, filename, file);
+            file.delete();
+            return s3Client.getUrl(BUCKET_NAME, filename).toString();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            throw new ValidationException("Error while uploading image");
+        }
+    }
+}
