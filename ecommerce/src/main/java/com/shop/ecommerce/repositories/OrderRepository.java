d4ba23773d110e743b29aee615e90352159f491b
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-07-02 23:15:27 -0300

    feat:implementing custom query dsl in repositories

--- ecommerce/src/main/java/com/shop/ecommerce/repositories/OrderRepository.java
+++ ecommerce/src/main/java/com/shop/ecommerce/repositories/OrderRepository.java
@@ -1,9 +1,10 @@
 package com.shop.ecommerce.repositories;
 
+import com.shop.ecommerce.enterprise.filter.CustomQuerydslPredicateExecutor;
 import com.shop.ecommerce.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long>, CustomQuerydslPredicateExecutor<Order> {
 }
--- ecommerce/src/main/java/com/shop/ecommerce/repositories/ProductRepository.java
+++ ecommerce/src/main/java/com/shop/ecommerce/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.shop.ecommerce.repositories;
 
+import com.shop.ecommerce.enterprise.filter.CustomQuerydslPredicateExecutor;
 import com.shop.ecommerce.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, CustomQuerydslPredicateExecutor<Product> {
 
     @Query("SELECT p FROM Product p ORDER BY price DESC")
     public List<Product> findProductsWithHighestPrice();
