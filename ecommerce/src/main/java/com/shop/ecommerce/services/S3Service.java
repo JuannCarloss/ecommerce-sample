387691a8266337ba5cbf112b37729242c26c9572
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-07-03 23:24:09 -0300

    fix: instantiating AmazonS3 instead of AmazonS3Client

--- ecommerce/src/main/java/com/shop/ecommerce/services/S3Service.java
+++ ecommerce/src/main/java/com/shop/ecommerce/services/S3Service.java
@@ -1,9 +1,9 @@
 package com.shop.ecommerce.services;
 
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.shop.ecommerce.enterprise.ValidationException;
 import com.shop.ecommerce.utils.MultipartfileConverter;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,11 +11,12 @@ import java.io.File;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class S3Service {
 
-    @Autowired
-    private AmazonS3Client s3Client;
     private static final String BUCKET_NAME = "ecommerce-products-imgs";
+    private final AmazonS3 s3Client;
+
 
     public String uploadImg(MultipartFile multipartFile){
         String filename = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
