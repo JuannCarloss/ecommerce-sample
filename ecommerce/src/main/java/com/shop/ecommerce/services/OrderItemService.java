b807d3bf6d244cee7bf778b9ada1f3f5fdcf8fa9
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2024-07-04 12:46:24 -0300

    refact: refactoring from Autowired to RequiredArgsConstructor and implementing ProductService class instead of ProductRepository

--- ecommerce/src/main/java/com/shop/ecommerce/services/OrderItemService.java
+++ ecommerce/src/main/java/com/shop/ecommerce/services/OrderItemService.java
@@ -3,23 +3,19 @@ package com.shop.ecommerce.services;
 import com.shop.ecommerce.enterprise.ValidationException;
 import com.shop.ecommerce.models.Order;
 import com.shop.ecommerce.models.OrderItem;
-import com.shop.ecommerce.models.Product;
 import com.shop.ecommerce.repositories.OrderItemRepository;
-import com.shop.ecommerce.repositories.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class OrderItemService {
 
-    @Autowired
-    private OrderItemRepository orderItemRepository;
+    private final OrderItemRepository orderItemRepository;
 
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductService productService;
 
     public void saveAll(List<OrderItem> orderItemList, Order order) {
         orderItemList.forEach(this::validateStock);
@@ -30,24 +26,21 @@ public class OrderItemService {
     }
 
     public void validateStock(OrderItem orderItem) {
-        var productById = getOptionalProduct(orderItem.getProduct().getId());
+        var productById = productService.getOptionalProduct(orderItem.getProduct().getId());
         if (orderItem.getProductQuantity() > productById.get().getStock()) {
             throw new ValidationException("We do not have stock for " + productById.get().getName() + " at the moment :(");
         }
         productById.get().setStock(productById.get().getStock() - orderItem.getProductQuantity());
     }
     public void totalValue(OrderItem orderItems, Order order) {
-        var productById = getOptionalProduct(orderItems.getProduct().getId());
+        var productById = productService.getOptionalProduct(orderItems.getProduct().getId());
         productById.ifPresent(product -> order.setTotalPrice(order.getTotalPrice() + (product.getPrice() * orderItems.getProductQuantity())));
     }
 
     public void validateProductQuantity(OrderItem orderItem){
         if (orderItem.getProductQuantity() <= 0){
-            var productById = getOptionalProduct(orderItem.getProduct().getId());
+            var productById = productService.getOptionalProduct(orderItem.getProduct().getId());
             throw new ValidationException("You need to have at least one of " + productById.get().getName() + " in your shop cart");
         }
-    }
-    private Optional<Product> getOptionalProduct(Long id) {
-        return productRepository.findById(id);
     }
 }
