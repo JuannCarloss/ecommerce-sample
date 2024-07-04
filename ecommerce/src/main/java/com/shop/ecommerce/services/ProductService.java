0868ac10d4f5cd8d7cf9355e911d9636993d2cd3
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2024-07-04 12:45:15 -0300

    refact: refactoring from Autowired to RequiredArgsConstructor and implementing getOptionalProduct method

--- ecommerce/src/main/java/com/shop/ecommerce/services/ProductService.java
+++ ecommerce/src/main/java/com/shop/ecommerce/services/ProductService.java
@@ -1,37 +1,27 @@
 package com.shop.ecommerce.services;
 
-import com.amazonaws.services.s3.AmazonS3;
 import com.shop.ecommerce.dtos.ProductRequestDTO;
 import com.shop.ecommerce.enterprise.NotFoundException;
-import com.shop.ecommerce.enterprise.ValidationException;
 import com.shop.ecommerce.models.Product;
 import com.shop.ecommerce.repositories.ProductRepository;
 import com.shop.ecommerce.strategy.NewProductValidationStrategy;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
-    @Autowired
-    private ModelMapper modelMapper;
-    @Autowired
-    private S3Service s3Service;
-
-    @Autowired
-    private ProductRepository repository;
+    private final ModelMapper modelMapper;
+    private final S3Service s3Service;
+    private final ProductRepository repository;
 
     @Autowired
     private List<NewProductValidationStrategy> productValidationStrategies;
@@ -96,5 +86,9 @@ public class ProductService {
 
     public void delete(Long id){
         repository.deleteById(id);
+    }
+
+    public Optional<Product> getOptionalProduct(Long id) {
+        return repository.findById(id);
     }
 }
