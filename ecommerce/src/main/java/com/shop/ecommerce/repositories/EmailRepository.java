526f3ea3e03653d4b296add0b982caf8bed27ec7
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2024-07-11 12:54:09 -0300

    refact: removing email class and repository

--- ecommerce/src/main/java/com/shop/ecommerce/repositories/EmailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shop.ecommerce.repositories;
-
-import com.shop.ecommerce.models.Email;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailRepository extends JpaRepository<Email, Long> {
-}
