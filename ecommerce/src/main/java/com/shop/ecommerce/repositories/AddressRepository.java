8551f1371598702a2cf45c7a1e7ee38d254dc480
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2024-07-11 12:54:50 -0300

    feat: address repository

---
+++ ecommerce/src/main/java/com/shop/ecommerce/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.shop.ecommerce.repositories;
+
+import com.shop.ecommerce.models.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
