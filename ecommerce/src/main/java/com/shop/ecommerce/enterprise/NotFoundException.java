a5653bb78c0d57aefc240c3cb1df615ebf1a46a6
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-07-12 18:08:47 -0300

    refact: changing name to OkNoContent

--- ecommerce/src/main/java/com/shop/ecommerce/enterprise/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.shop.ecommerce.enterprise;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message){
-        super(message);
-    }
-}
---
+++ ecommerce/src/main/java/com/shop/ecommerce/enterprise/OkNoContent.java
@@ -0,0 +1,7 @@
+package com.shop.ecommerce.enterprise;
+
+public class OkNoContent extends RuntimeException{
+    public OkNoContent(String message){
+        super(message);
+    }
+}
