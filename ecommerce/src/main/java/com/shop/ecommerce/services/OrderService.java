5b19103da08c5df85fd5bcee5cfa3af8f51d9d03
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-07-16 23:23:20 -0300

    feat: implementing kafka template to send messages

--- ecommerce/src/main/java/com/shop/ecommerce/services/OrderService.java
+++ ecommerce/src/main/java/com/shop/ecommerce/services/OrderService.java
@@ -1,13 +1,17 @@
 package com.shop.ecommerce.services;
 
+import com.shop.ecommerce.dtos.OrderToPaymentDTO;
+import com.shop.ecommerce.models.Customer;
 import com.shop.ecommerce.models.Order;
+import com.shop.ecommerce.repositories.CustomerRepository;
 import com.shop.ecommerce.repositories.OrderRepository;
 import com.shop.ecommerce.strategy.NewOrderValidationStrategy;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
+import java.util.Optional;
 
 
 @Service
@@ -17,15 +21,24 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final OrderItemService orderItemService;
     private final NewOrderValidationStrategy orderValidationStrategy;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final CustomerRepository customerRepository;
+    private static final String TOPIC = "order-payment";
 
     @Transactional
     public Order post(Order entity){
 
         orderValidationStrategy.validate(entity);
 
-        var save = orderRepository.save(entity);
+        var order = orderRepository.save(entity);
         orderItemService.saveAll(entity.getOrderItem(), entity);
 
-        return save;
+        Optional<Customer> byID = customerRepository.findById(order.getCustomer().getId());
+
+        var payment = new OrderToPaymentDTO(order.getId(), byID.get().getEmail(), order.getTotalPrice());
+
+        kafkaTemplate.send(TOPIC, payment);
+
+        return order;
     }
 }
