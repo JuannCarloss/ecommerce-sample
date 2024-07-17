d81dac6ac409dddd2b67b74104bf6f8d1c28100e
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-07-16 21:14:56 -0300

    feat: Kafka health checker

---
+++ ecommerce/src/main/java/com/shop/ecommerce/resource/KafkaHealthCheckController.java
@@ -0,0 +1,23 @@
+package com.shop.ecommerce.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaHealthCheckController {
+
+    private static final String TOPIC = "order-payment";
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+
+    @PostMapping
+    public String message(String message){
+        kafkaTemplate.send(TOPIC, message);
+        return "message sent";
+    }
+}
