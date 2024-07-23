7c279b7999f34c28fe509c28ddcde6c7178e18f4
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-07-22 23:18:17 -0300

    feat: enabling feign clients

--- ecommerce/src/main/java/com/shop/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/shop/ecommerce/EcommerceApplication.java
@@ -3,11 +3,13 @@ package com.shop.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableCaching
+@EnableFeignClients
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
